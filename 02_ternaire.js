37c31315cdb38dc576df314f6f3074697e8fe43c
Steven RIVIERE <sriver@hotmail.fr>
2024-05-13 13:33:18 +0200

    test

--- 02_ternaire.js
+++ 02_ternaire.js
@@ -6,8 +6,10 @@
  *    - utiliser l'opérateur ternaire (if interdit)
  */
 function ternaire(a , b) {
+    return (b>a)? "b supérieur à a"  : "b n'est pas supérieur à a";
 }
 
 console.log(ternaire(1, 2));
+
 
 module.exports = ternaire;
