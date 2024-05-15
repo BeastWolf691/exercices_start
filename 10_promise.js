a792ceef9d111fe0c61bbf42f5171a7a1653bfca
Steven RIVIERE <sriver@hotmail.fr>
2024-05-15 11:30:08 +0200

    test

--- 10_promise.js
+++ 10_promise.js
@@ -6,14 +6,12 @@
  */
 
 
-const sleep = () => {return new Promise((resolve,reject) => 
-  {setTimeout(() => {
-    resolve();},
-    2000);})};
-
-sleep()
-    .then(() => console.log('done'))
-    .catch(() => console.log('error'));
-    
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const asynchrone = async () => {
+  console.log("start");
+  await sleep(2000);
+  console.log("end");
+}
+ asynchrone();
 /**dans cet exemple end sera affiché après start sous un délai de 2000ms  */
 module.exports = {sleep};
